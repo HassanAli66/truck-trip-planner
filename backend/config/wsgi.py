8a5f8681a209c1b3eb7191537260054f74da35db
Hassan ali <il.magnifico@icloud.com>
2026-06-26 20:30:05 +0300

    Update wsgi.py

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
+
+# Calculate the path to the 'backend' directory dynamically
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Inject the 'backend' directory into Python's search paths
+if str(BASE_DIR) not in sys.path:
+    sys.path.append(str(BASE_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
